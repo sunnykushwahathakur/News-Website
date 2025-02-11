a2d29a52ab64cea85648df416e94f0964aedc97b
sunnykushwahathakur <sunnykushwahathakur@gmail.com>
2025-02-12 04:54:11 +0530

    back to strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { HashRouter } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
 )
